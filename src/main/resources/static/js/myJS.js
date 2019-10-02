8edfe8a371023ec6cacacc1020cbddc0045af67a
Remy Bourdoncle <S226294@siege.axa-fr.intraxa>
2019-10-02 17:37:37 +0200

    Ajout de la gestion des avertissements avant suppression

---
+++ src/main/resources/static/js/myJS.js
@@ -0,0 +1,8 @@
+// Alerte suppression
+function confirmer_suppression()
+{
+    if(confirm('ATTENTION ! Vous allez supprimer un élément, voulez-vous poursuivre ?'))
+    { return true; }
+    else
+    { return false; }
+}
